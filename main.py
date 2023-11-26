7ca9aa4a7fb9a2ef93da0c81837366bcd1ce950f
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-11-26 02:14:19 +0100

    Refactor generate_password and remove save_password functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from generate_password import generate_password, save_password
+from generate_password import generate_password
 
 
 def main():
@@ -8,6 +8,7 @@ def main():
     st.info("Choose your password length")
     length = st.slider("Password Length", min_value=6, max_value=30, value=12)
 
+    st.info("Select the character types to include")
     use_lowercase = st.checkbox("Use Lowercase Letters")
     use_uppercase = st.checkbox("Use Uppercase Letters")
     use_digits = st.checkbox("Use Digits")
@@ -17,12 +18,7 @@ def main():
         password = generate_password(length, use_lowercase, use_uppercase, use_digits, use_special_chars)
         if password:
             st.success("Generated Password: {}".format(password))
-
-            save_button = st.button("save password")
-
-            if save_button:
-                save_password(password)
-                st.success("Password saved successfully.")
+            st.text_input("Copy the password from here", value=password)
 
 
 if __name__ == "__main__":
